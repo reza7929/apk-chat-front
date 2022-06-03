4bfb1052f8ef801cba5e70a7ecb6085a8e2bbf2c
Reza Mohammadi <rezahd7929@gmail.com>
2022-06-03 22:49:52 +0430

    fixed bug

--- pages/auth.js
+++ pages/auth.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import LoginDataSet from "../components/auth/login-data-set";
 import RegisterDataSet from "../components/auth/register-data-set";
-import AuthLayout from "../layouts/login-layout";
+import AuthLayout from "../layouts/auth-layout";
 
 export default function Auth() {
   const [isRegister, setIsRegister] = useState(false);
