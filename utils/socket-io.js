62255477b0f7eae34d660c7e8fede789d1407883
Reza Mohammadi <rezahd7929@gmail.com>
2022-06-03 22:37:43 +0430

    merge token-auth codes with socket.io

--- utils/socket-io.js
+++ utils/socket-io.js
@@ -1,13 +1,25 @@
 import jwt_decode from "jwt-decode";
 import io from "socket.io-client";
 import { api_backend } from "./constance";
+import Router from "next/router";
+import axios from "axios";
 
-export const socketIO = () => {
+export const socketIO = async () => {
   if (typeof window === "undefined") return;
-  const token = localStorage.getItem("token");
-  const decoded = jwt_decode(token);
-  const socket = io(api_backend, {
-    query: `fromID=${decoded.id}`,
-  });
-  return socket;
+  const token = window.localStorage.getItem("token");
+  if (!token) return await Router.push("/auth");
+  try {
+    await axios.post(api_backend + "/auth", {
+      token: window.localStorage.getItem("token"),
+    });
+    const decoded = jwt_decode(token);
+    const socket = io(api_backend, {
+      query: `fromID=${decoded.id}`,
+    });
+    setIsPageReady(true);
+    return socket;
+  } catch (err) {
+    console.log(err);
+    return await Router.push("/auth");
+  }
 };
