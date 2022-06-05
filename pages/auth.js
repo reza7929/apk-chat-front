149cced8a49c1052d4c7b07f7db5dd9489b862e0
Reza Mohammadi <rezahd7929@gmail.com>
2022-06-06 01:35:44 +0430

    add headers for pages

--- pages/auth.js
+++ pages/auth.js
@@ -2,12 +2,17 @@ import { useState } from "react";
 import LoginDataSet from "../components/auth/login-data-set";
 import RegisterDataSet from "../components/auth/register-data-set";
 import AuthLayout from "../layouts/auth-layout";
+import Head from "next/head";
 
 export default function Auth() {
   const [isRegister, setIsRegister] = useState(false);
 
   return (
     <AuthLayout>
+      <Head>
+        <title>apk | احرازهویت</title>
+        <meta name="description" content="پروژه تست احرازهویت" />
+      </Head>
       {isRegister ? (
         <RegisterDataSet
           isRegister={isRegister}
