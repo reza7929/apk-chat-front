b75f4e25d39ff9fffbbaf125f1f97b3c804d72c6
Reza Mohammadi <rezahd7929@gmail.com>
2022-06-29 12:18:46 +0430

    added eui provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@elastic/eui/dist/eui_theme_light.css";
+
+import { EuiProvider } from "@elastic/eui";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <EuiProvider>
+      <Component {...pageProps} />
+    </EuiProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
